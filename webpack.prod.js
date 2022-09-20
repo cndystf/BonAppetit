51320eb25852a8297633c6a272c80a4c8a0c21cd
unknown <cndysteff@gmail.com>
2022-09-21 04:34:53 +0700

    Initial commit, next will do styling

---
+++ webpack.prod.js
@@ -0,0 +1,22 @@
+const {merge} = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'esbuild-loader',
+            options: {
+              target: 'es2015'
+            }
+          }
+        ]
+      }
+    ]
+  }
+});
